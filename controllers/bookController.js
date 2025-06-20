b70645df756dee3315d5997ab87dda9ebb5821d7
Mounir-Bmz <mounir.boumaza.dev@gmail.com>
2025-06-20 12:24:04 +0200

    Note moyenne d'un livre

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,11 +2,21 @@ const fs = require('fs');
 const Book = require('../models/book');
 
 exports.getAllBooks = (req, res) => {
-  Book.find().then(books => res.status(200).json(books)).catch(error => res.status(400).json({ error }));
+  Book.find()
+    .then(books => res.status(200).json(books.map(book => ({
+      ...book.toObject(),
+      voteCount: book.ratings.length
+    }))))
+    .catch(error => res.status(400).json({ error }));
 };
 
 exports.getOneBook = (req, res) => {
-  Book.findOne({ _id: req.params.id }).then(book => res.status(200).json(book)).catch(error => res.status(404).json({ error }));
+  Book.findOne({ _id: req.params.id })
+    .then(book => {
+      if (!book) return res.status(404).json({ error: 'Book not found' });
+      res.status(200).json({ ...book.toObject(), voteCount: book.ratings.length });
+    })
+    .catch(error => res.status(404).json({ error }));
 };
 
 exports.createBook = (req, res) => {
